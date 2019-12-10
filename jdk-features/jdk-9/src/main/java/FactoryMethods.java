e9742a22740a1cc74ac2eb6f81ece08f1983a22d
Kevin Lange <kevin.lange@neos-it.de>
2019-12-10 13:31:47 +0100

    immutable Set, List, Map

--- jdk-features/jdk-9/src/main/java/FactoryMethods.java
+++ jdk-features/jdk-9/src/main/java/FactoryMethods.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class FactoryMethods {
 
@@ -8,5 +10,29 @@ public class FactoryMethods {
 
     public void nonEmptyList() {
         List<String> immutableList = List.of("A", "B");
+    }
+
+    /**
+     * @see https://www.journaldev.com/12984/javase9-factories-for-immutable-set
+     */
+    public void emptySet() {
+        Set immutableSet = Set.of();
+    }
+
+    public void nonEmptySet() {
+        Set<String> immutableSet = Set.of("one","two","three");
+    }
+
+    public void nonEmptySetVarargs() {
+        String[] nameArr =  { "one", "two", "three"};
+        Set<String[]> set= Set.<String[]>of(nameArr);
+    }
+
+    public void emptyMap() {
+        Map emptyImmutableMap = Map.of();
+    }
+
+    public void nonEmptyMap(){
+        Map nonemptyImmutableMap = Map.of(1, "one", 2, "two", 3, "three");
     }
 }
